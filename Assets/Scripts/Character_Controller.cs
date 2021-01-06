6a613f4a64963da3041cf41878a673b6c5e521ec
DESKTOP-QJTLJIP\denze <denzelstellingwerf@outlook.com>
2021-01-06 19:40:08 +0100

    Fixed PostFX. Added magazine logic

--- Assets/Scripts/Character_Controller.cs
+++ Assets/Scripts/Character_Controller.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Character_Controller : MonoBehaviour
 {
     Animator m_Animator;
-    AudioSource m_shootingSound; 
+    AudioSource m_shootingSound;
+    public int magazine = 7;
+    public bool magazineEmpty = false; 
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +21,38 @@ public class Character_Controller : MonoBehaviour
     {
       if (Input.GetButtonDown("Fire1"))
         {
-            m_Animator.SetTrigger("Fire");
-            m_shootingSound.Play();
-            Debug.Log("Fire Animation");
+            
+
+            if (magazine == 0)
+            {
+                magazineEmpty = true; 
+            }
+
+            else
+            {
+                magazine = magazine - 1;
+                magazineEmpty = false;
+            }
+
+            if (magazineEmpty == true)
+            {
+                Debug.Log("Empty mag");
+            }
+
+            else
+            {
+                m_Animator.SetTrigger("Fire");
+                m_shootingSound.Play();
+                Debug.Log("Fire Animation");
+            }
+            
         }
 
         if (Input.GetKeyDown("r"))
         {
+
             m_Animator.SetTrigger("Reload");
+            magazine = 7;
             Debug.Log("Reload Animation");
         }
     }
