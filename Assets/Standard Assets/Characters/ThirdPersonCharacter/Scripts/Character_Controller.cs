3d4d3df24d2c6360ec8a7a199dac51d83f930eb0
DESKTOP-QJTLJIP\denze <denzelstellingwerf@outlook.com>
2021-01-09 21:50:09 +0100

    Added death animations and impact animations. Impact animation is disabled.

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/Character_Controller.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/Character_Controller.cs
@@ -2,102 +2,109 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character_Controller : MonoBehaviour
-{
-    Animator m_Animator;
-    Animator target_animator;
-    AudioSource m_shootingSound;
 
-    //variables for magazine logic
-    public int magazine = 7;
-    public bool magazineEmpty = false;
-
-    //variables for raycast
-    public float damage = 10f;
-    public float range = 100f;
-
-    public Camera fpsCam; 
-
-    // Start is called before the first frame update
-    void Start()
+    public class Character_Controller : MonoBehaviour
     {
-        m_Animator = gameObject.GetComponent<Animator>();
-        m_shootingSound = GetComponent<AudioSource>();
-    }
+        Animator m_Animator;
+        Animator target_animator;
+        AudioSource m_shootingSound;
 
-    // Update is called once per frame
-    void Update()
-    {
-      if (Input.GetButtonDown("Fire2"))
+        //variables for magazine logic
+        public int magazine = 7;
+        public bool magazineEmpty = false;
+
+        //variables for raycast
+        public float damage = 10f;
+        public float range = 100f;
+
+        public Camera fpsCam;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            m_Animator.SetBool("isAiming", true);
-        }
-      if (Input.GetButtonUp("Fire2"))
-        {
-            m_Animator.SetBool("isAiming", false);
-        }
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-            //raycast shooting logic function
-            Shoot();
-
-            if (magazine == 0)
-            {
-                magazineEmpty = true; 
-            }
-
-            else
-            {
-                magazine = magazine - 1;
-                magazineEmpty = false;
-            }
-
-            if (magazineEmpty == true)
-            {
-                Debug.Log("Empty mag");
-            }
-
-            else
-            {
-                m_Animator.SetTrigger("Fire");
-                m_shootingSound.Play();
-                Debug.Log("Fire Animation");
-            }
-            
+            m_Animator = gameObject.GetComponent<Animator>();
+            m_shootingSound = GetComponent<AudioSource>();
         }
 
-        if (Input.GetKeyDown("r"))
+        // Update is called once per frame
+        void Update()
         {
-
-            m_Animator.SetTrigger("Reload");
-            magazine = 7;
-            Debug.Log("Reload Animation");
-        }
-    }
-
-    void Shoot ()
-    {
-        RaycastHit hit; 
-        
-        if (magazineEmpty == false)
-        {
-            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+            if (Input.GetButtonDown("Fire2"))
             {
-                Target target = hit.transform.GetComponent<Target>();
+                m_Animator.SetBool("isAiming", true);
+            }
+            if (Input.GetButtonUp("Fire2"))
+            {
+                m_Animator.SetBool("isAiming", false);
+            }
 
-                if (target != null)
+            if (Input.GetButtonDown("Fire1"))
+            {
+                //raycast shooting logic function
+                Shoot();
+
+                if (magazine == 0)
                 {
-                    target.TakeDamage(damage);
+                    magazineEmpty = true;
+                }
 
-                    GameObject armature = target.transform.GetChild(0).gameObject;
+                else
+                {
+                    magazine = magazine - 1;
+                    magazineEmpty = false;
+                }
 
-                    target_animator = armature.GetComponent<Animator>();
+                if (magazineEmpty == true)
+                {
+                    Debug.Log("Empty mag");
+                }
 
-                    target_animator.SetTrigger("Hit");
+                else
+                {
+                    m_Animator.SetTrigger("Fire");
+                    m_shootingSound.Play();
+                    Debug.Log("Fire Animation");
+                }
+
+            }
+
+            if (Input.GetKeyDown("r"))
+            {
+
+                m_Animator.SetTrigger("Reload");
+                magazine = 7;
+                Debug.Log("Reload Animation");
+            }
+        }
+
+        void Shoot()
+        {
+            RaycastHit hit;
+
+            //makes sure player can't shoot with empty mag
+            if (magazineEmpty == false)
+            {
+                //shoots a ray
+                if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+                {
+                    Target target = hit.transform.GetComponent<Target>();
+
+                    if (target != null)
+                    {
+                        //raycast target takes damage
+                        target.TakeDamage(damage);
+
+                        //set trigger 'hit' at child of zombie.
+                        GameObject armature = target.transform.GetChild(0).gameObject;
+
+                        target_animator = armature.GetComponent<Animator>();
+
+                        target_animator.SetTrigger("Hit");
+                    }
                 }
             }
+
         }
-        
     }
-}
+
+
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/Target.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/Target.cs
@@ -2,36 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Target : MonoBehaviour
-{
-    //health of zombie 
-    public float health = 50f;
 
-    // Start is called before the first frame update
-    void Start()
+    public class Target : MonoBehaviour
     {
-        
-    }
+        public Rigidbody rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    //regulates health and damage taken
-    public void TakeDamage(float amount)
-    {
-        health -= amount;
-        if (health <= 0f)
+        //health of zombie 
+        public float health = 50f;
+
+        //time it takes for zombie to despawn after death.
+        public float despawnDelay = 1f;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            Die();
+
         }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        //regulates health and damage taken
+        public void TakeDamage(float amount)
+        {
+            health -= amount;
+            if (health <= 0f)
+            {
+                Die();
+            }
+        }
+
+        void Die()
+        {
+            //Destroy(gameObject);
+            GameObject armature = gameObject.transform.GetChild(0).gameObject;
+
+            Animator target_animator = armature.GetComponent<Animator>();
+
+            target_animator.SetBool("isDead", true);
+
+            //ZombieCharactercript script = gameObject.GetComponent<ZombieCharacterScript>();
+
+            rb = GetComponent<Rigidbody>();
+            //destroys gameobject
+            Destroy(gameObject, despawnDelay);
+            rb.velocity = Vector3.zero;
+        }
+
     }
 
-    void Die()
-    {
-        Destroy(gameObject);
-    }
 
-}
+
