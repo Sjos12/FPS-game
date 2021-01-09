2082d3d12fd72e17a937ab405e2b66acc62e9098
DESKTOP-QJTLJIP\denze <denzelstellingwerf@outlook.com>
2021-01-09 11:59:32 +0100

    Added weapon sway to character.

--- Assets/Scripts/Weapon_Sway.cs
+++ Assets/Scripts/Weapon_Sway.cs
@@ -6,45 +6,29 @@ public class Weapon_Sway : MonoBehaviour
 {
 
     public float amount;
-    public float maxamount;
     public float smooth;
-    private Quaternion def;
-    private bool Paused = false;
+
+    public bool paused = false; 
+
+    private Vector3 def;
+
+
 
     void Start()
     {
-        def = transform.localRotation;
+        def = transform.localPosition;
     }
 
     void Update()
     {
-        float factorX = (Input.GetAxis("Mouse Y")) * amount;
-        float factorY = -(Input.GetAxis("Mouse X")) * amount;
-        //float factorZ = -Input.GetAxis("Vertical") * amount;
-        float factorZ = 0 * amount;
-
-        if (!Paused)
+        if (!paused)
         {
-            if (factorX > maxamount)
-                factorX = maxamount;
+            float factorX = -Input.GetAxis("Mouse X") * amount;
+            float factorY = -Input.GetAxis("Mouse Y") * amount;
 
-            if (factorX < -maxamount)
-                factorX = -maxamount;
-
-            if (factorY > maxamount)
-                factorY = maxamount;
-
-            if (factorY < -maxamount)
-                factorY = -maxamount;
-
-            if (factorZ > maxamount)
-                factorZ = maxamount;
-
-            if (factorZ < -maxamount)
-                factorZ = -maxamount;
-
-            Quaternion Final = Quaternion.Euler(def.x + factorX, def.y + factorY, def.z + factorZ);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, Final, (Time.time * smooth));
+            Vector3 finalPosition = new Vector3(factorX, factorY, 0);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition + def, Time.deltaTime * smooth);
         }
+        
     }
 }
